e2964908107ea88a5971a436a48aa948006e7733
bizz84 <bizz84@gmail.com>
2020-02-03 22:13:19 +0000

    Refresh all data at once

--- lib/app/api/api_repository.dart
+++ lib/app/api/api_repository.dart
@@ -1,6 +1,21 @@
 import 'package:ncov_2019_app_flutter/app/api/api.dart';
 import 'package:ncov_2019_app_flutter/app/api/api_service.dart';
 
+class Data {
+  Data({this.values});
+
+  final Map<Endpoint, int> values;
+  int get cases => values[Endpoint.cases];
+  int get casesSuspected => values[Endpoint.casesSuspected];
+  int get casesConfirmed => values[Endpoint.casesConfirmed];
+  int get deaths => values[Endpoint.deaths];
+  int get recovered => values[Endpoint.recovered];
+
+  @override
+  String toString() =>
+      'cases: $cases, suspected: $casesSuspected, confirmed: $casesConfirmed, deaths: $deaths, recovered: $recovered';
+}
+
 class APIRepository {
   APIRepository(this.apiService);
   final APIService apiService;
@@ -18,5 +33,35 @@ class APIRepository {
       _token = await apiService.getToken();
       return await apiService.getData(token: _token, endpoint: endpoint);
     }
+  }
+
+  Future<Data> getAllData() async {
+    if (_token == null) {
+      _token = await apiService.getToken();
+    }
+    try {
+      return await _getAllData();
+    } catch (e) {
+      // if unauthorized, get token again
+      _token = await apiService.getToken();
+      return await _getAllData();
+    }
+  }
+
+  Future<Data> _getAllData() async {
+    final results = await Future.wait([
+      apiService.getData(token: _token, endpoint: Endpoint.cases),
+      apiService.getData(token: _token, endpoint: Endpoint.casesSuspected),
+      apiService.getData(token: _token, endpoint: Endpoint.casesConfirmed),
+      apiService.getData(token: _token, endpoint: Endpoint.deaths),
+      apiService.getData(token: _token, endpoint: Endpoint.recovered),
+    ]);
+    return Data(values: {
+      Endpoint.cases: results[0],
+      Endpoint.casesSuspected: results[1],
+      Endpoint.casesConfirmed: results[2],
+      Endpoint.deaths: results[3],
+      Endpoint.recovered: results[4],
+    });
   }
 }
--- lib/app/dashboard.dart
+++ lib/app/dashboard.dart
@@ -9,9 +9,15 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  Data _data;
+
   @override
   void initState() {
     super.initState();
+    _updateData();
   }
 
   static Map<Endpoint, String> _titles = {
@@ -22,63 +28,55 @@ class _DashboardState extends State<Dashboard> {
     Endpoint.recovered: 'Recovered',
   };
 
+  Future<void> _refresh() async {
+    _refreshIndicatorKey.currentState.show();
+    _updateData();
+  }
+
+  Future<void> _updateData() async {
+    final apiRepository = Provider.of<APIRepository>(context, listen: false);
+    final data = await apiRepository.getAllData();
+    setState(() => _data = data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('nCoV 2019 Tracker'),
       ),
-      body: Column(
-        children: [
-          for (var endpoint in Endpoint.values)
-            APIResultCard(
-              title: _titles[endpoint],
-              endpoint: endpoint,
-            ),
-        ],
+      body: RefreshIndicator(
+        key: _refreshIndicatorKey,
+        onRefresh: _refresh,
+        child: ListView(
+          children: [
+            for (var endpoint in Endpoint.values)
+              APIResultCard(
+                title: _titles[endpoint],
+                value: _data != null ? _data.values[endpoint] : null,
+              ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class APIResultCard extends StatefulWidget {
-  const APIResultCard({Key key, this.title, this.endpoint}) : super(key: key);
+class APIResultCard extends StatelessWidget {
+  const APIResultCard({Key key, this.title, this.value}) : super(key: key);
   final String title;
-  final Endpoint endpoint;
-
-  @override
-  _APIResultCardState createState() => _APIResultCardState();
-}
-
-class _APIResultCardState extends State<APIResultCard> {
-  int value;
-
-  @override
-  void initState() {
-    super.initState();
-    updateData();
-  }
-
-  Future<void> updateData() async {
-    try {
-      final apiRepository = Provider.of<APIRepository>(context, listen: false);
-      final data = await apiRepository.getData(widget.endpoint);
-      setState(() => value = data);
-    } catch (e) {
-      print(e);
-    }
-  }
+  final int value;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
       child: Card(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: <Widget>[
-              Text(widget.title, style: Theme.of(context).textTheme.headline),
+              Text(title, style: Theme.of(context).textTheme.headline),
               Expanded(child: Container()),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -87,10 +85,6 @@ class _APIResultCardState extends State<APIResultCard> {
                     value != null ? value.toString() : '',
                     style: Theme.of(context).textTheme.display1,
                   ),
-                  RaisedButton(
-                    child: Text('Refresh'),
-                    onPressed: updateData,
-                  )
                 ],
               )
             ],
