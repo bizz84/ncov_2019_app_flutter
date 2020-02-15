56a70a51f918a951156da1f9885601e8c87fafdd
bizz84 <bizz84@gmail.com>
2020-02-15 18:23:14 +0000

    Initialize date formatting when running main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+//import 'package:intl/intl.dart';
 import 'package:ncov_2019_app_flutter/app/api/api.dart';
 import 'package:ncov_2019_app_flutter/app/api/api_repository.dart';
 import 'package:ncov_2019_app_flutter/app/api/api_service.dart';
 import 'package:ncov_2019_app_flutter/app/ui/dashboard.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  //Intl.defaultLocale = 'en_GB';
+  await initializeDateFormatting();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
